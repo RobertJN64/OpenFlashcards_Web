d641bdc57945d6860afda6a4698751765e675196
RobertJN64 <robertjnies@gmail.com>
2023-07-10 02:23:46 -0400

    MC ?s working

--- src/App.js
+++ src/App.js
@@ -7,45 +7,112 @@ import './App.css';
 
 const app_status = {
   WaitingOnStart: Symbol("WaitingOnStart"),
-  AskMC: Symbol("AskMC"),
-  AskOR: Symbol("AskOR"),
+  AskQuestion: Symbol("AsQuestion"),
+  Correct: Symbol("Correct"),
+  Incorrect: Symbol("Incorrect"),
   End: Symbol("End"),
 }
 
-function e_btn(x) {
-  alert("Button clicked: " + x);
+const MODE_DEF = true; // answer with def
+const MODE_TERM = false;  // answer with term
+
+var mode = MODE_DEF; //TODO - allow mode switching
+//TODO - missed words
+//TODO - stopflag
+
+var cards = {};
+var progress = {};
+var canswer = ""; //correct answer
+
+// window.addEventListener(
+//   "keydown",
+//   (event) => {
+//     if (['1', '2', '3', '4'].includes(event.key)) {
+//       e_btn(event.key);
+//     }
+//   },
+//   true,
+// );
+
+
+
+function shuffleArray(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
+  }
 }
-
-window.addEventListener(
-  "keydown",
-  (event) => {
-    if (['1', '2', '3', '4'].includes(event.key)) {
-      e_btn(event.key);
-    }
-  },
-  true,
-);
-
-function e_btn_1() { e_btn(1) };
-function e_btn_2() { e_btn(2) };
-function e_btn_3() { e_btn(3) };
-function e_btn_4() { e_btn(4) };
-
 
 function MainCard() {
   const [current_status, set_current_status] = useState(app_status.WaitingOnStart);
 
   function e_start() {
-    set_current_status(app_status.AskMC);
+    set_current_status(app_status.AskQuestion);
   }
 
-  function render_start_screen() {
+  function StartScreen() {
+    const [upload_card_status, set_upload_card_status] = useState("Waiting on upload...");
+
+    const handleCardsUpload = e => {
+      const fileReader = new FileReader();
+      fileReader.readAsText(e.target.files[0], "UTF-8");
+      fileReader.onload = e => {
+        var lines = e.target.result.split('\n');
+        for (var line of lines) {
+          line = line.trim();
+          if (!line.includes(' | ')) {
+            alert("<" + line + "> did not contain ` | `");
+          }
+          var term = line.split(' | ')[0];
+          var definition = line.split(' | ')[1];
+
+          if (mode) {
+            cards[term] = definition;
+          }
+          else {
+            cards[definition] = term
+          }
+        }
+        console.log(cards);
+        set_upload_card_status("Found: " + Object.keys(cards).length + " cards.");
+
+        var delkeys = []
+        var addkeys = []
+        for (const key of Object.keys(progress)) {
+          if (!(key in cards)) {
+            delkeys.push(key);
+          }
+        }
+
+        for (const key of Object.keys(cards)) {
+          if (!(key in progress)) {
+            addkeys.push(key);
+          }
+        }
+
+        for (const key of delkeys) {
+          delete progress[key];
+        }
+
+        for (const key of addkeys) {
+          progress[key] = {
+            "mc": false, "or": false,
+            "missed": false
+          }; //multichoice, open response
+        }
+        console.log(progress);
+      };
+    };
+
     return (
       <>
         <Card style={{ width: '50%', marginLeft: 'auto', marginRight: 'auto' }}>
           <Card.Body>
 
             <Card.Title>Load your cards...</Card.Title>
+            <br />
+            <input type="file" onChange={handleCardsUpload} />
+            <p>{upload_card_status}</p>
             <br />
             <Button style={{ width: '100%' }} className='btn-success' onClick={e_start}>Start</Button>
           </Card.Body>
@@ -55,26 +122,61 @@ function MainCard() {
     )
   }
 
-  function ask_mc(term, cards, progress) {
+  function ask_mc(term, cards, perc) {
+    var answers = [cards[term]];
+
+    function e_btn(x) {
+      if (answers[x - 1] === cards[term]) {
+        if (progress[term]["missed"]) {
+          progress[term]["missed"] = false;
+        }
+        else {
+          progress[term]["mc"] = true;
+        }
+        set_current_status(app_status.Correct);
+      }
+      else {
+        progress[term]["missed"] = true;
+        canswer = cards[term];
+        set_current_status(app_status.Incorrect);
+      }
+    }
+
+    function e_btn_1() { e_btn(1) };
+    function e_btn_2() { e_btn(2) };
+    function e_btn_3() { e_btn(3) };
+    function e_btn_4() { e_btn(4) };
+
+
+    while (answers.length < 4) {
+      var c = answers[0];
+      while (answers.includes(c)) {
+        c = Object.values(cards)[Math.floor((Math.random() * Object.values(cards).length))];
+      }
+      answers.push(c);
+    }
+
+    shuffleArray(answers);
+
     return (
       <>
         <Card style={{ width: '50%', marginLeft: 'auto', marginRight: 'auto' }}>
           <Card.Body>
-            <p style={{ textAlign: 'right' }}>{progress.toFixed(4) * 100}%</p>
+            <p style={{ textAlign: 'right' }}>{perc.toFixed(4) * 100}%</p>
             <Card.Title>{term}</Card.Title>
             <br />
             <table style={{ width: '100%' }}>
               <tbody>
                 <tr>
-                  <td><Button style={{ width: '100%' }} onClick={e_btn_1}>Item 1</Button></td>
+                  <td><Button style={{ width: '100%' }} onClick={e_btn_1}>{answers[0]}</Button></td>
                   <td></td>
-                  <td><Button style={{ width: '100%' }} onClick={e_btn_2}>Item 2</Button></td>
+                  <td><Button style={{ width: '100%' }} onClick={e_btn_2}>{answers[1]}</Button></td>
                 </tr>
                 <tr><td><p></p></td></tr>
                 <tr>
-                  <td><Button style={{ width: '100%' }} onClick={e_btn_3}>Item 3</Button></td>
+                  <td><Button style={{ width: '100%' }} onClick={e_btn_3}>{answers[2]}</Button></td>
                   <td></td>
-                  <td><Button style={{ width: '100%' }} onClick={e_btn_4}>Item 4</Button></td>
+                  <td><Button style={{ width: '100%' }} onClick={e_btn_4}>{answers[3]}</Button></td>
                 </tr>
               </tbody>
             </table>
@@ -85,12 +187,96 @@ function MainCard() {
     )
   }
 
+  function AskQuestion() {
+    var mc_list = []
+    for (const key of Object.keys(progress)) {
+      if (!progress[key]['mc']) {
+        mc_list.push(key);
+      }
+    }
+
+    if (mc_list.length > 0) {
+      var term = mc_list[Math.floor((Math.random() * mc_list.length))];
+      return ask_mc(term, cards, 1 - mc_list.length / Object.values(progress).length);
+    }
+    else {
+      set_current_status(app_status.Done); //TODO - OR
+    }
+  }
+
+  function Correct() {
+    setTimeout(() => set_current_status(app_status.AskQuestion), 1000)
+    return (
+      <>
+        <Card style={{ width: '50%', marginLeft: 'auto', marginRight: 'auto' }}>
+          <Card.Body>
+            <Card.Title>Correct!</Card.Title>
+          </Card.Body>
+        </Card >
+      </>
+    )
+  }
+
+  function Incorrect() {
+    function e_inc_cont() {
+      set_current_status(app_status.AskQuestion);
+    }
+
+    return (
+      <>
+        <Card style={{ width: '50%', marginLeft: 'auto', marginRight: 'auto' }}>
+          <Card.Body>
+            <Card.Title>Incorrect!</Card.Title>
+            <br />
+            <p className="text-center">{canswer}</p>
+            <Button style={{ width: '100%' }} className='btn-danger' onClick={e_inc_cont}>Continue</Button>
+          </Card.Body>
+        </Card >
+      </>
+    )
+  }
+
+  function Done() {
+    function e_restart() {
+      set_current_status(app_status.WaitingOnStart);
+    }
+
+    function e_again() {
+      for (const key of Object.keys(progress)) {
+        progress[key] = {
+          "mc": false, "or": false,
+          "missed": false
+        }; //multichoice, open response
+      }
+      set_current_status(app_status.AskQuestion);
+    }
+
+    return (
+      <>
+        <Card style={{ width: '50%', marginLeft: 'auto', marginRight: 'auto' }}>
+          <Card.Body>
+            <Card.Title>Done!</Card.Title>
+            <br />
+            <Button style={{ width: '100%' }} className='btn-success' onClick={e_restart}>New Set!</Button>
+            <p></p>
+            <Button style={{ width: '100%' }} className='btn-success' onClick={e_again}>Study again!</Button>
+          </Card.Body>
+        </Card >
+      </>
+    )
+  }
 
   switch (current_status) {
     case app_status.WaitingOnStart:
-      return render_start_screen();
-    case app_status.AskMC:
-      return ask_mc("Example Term", [], 17 / 39)
+      return <StartScreen />
+    case app_status.AskQuestion:
+      return <AskQuestion />
+    case app_status.Correct:
+      return <Correct />
+    case app_status.Incorrect:
+      return <Incorrect />
+    case app_status.Done:
+      return <Done />
     default:
       alert("App status invalid! " + current_status)
       break;
@@ -101,8 +287,6 @@ const App = () => (
   <>
     <h1 className="text-center">Open Flashcards Web</h1>
     <MainCard />
-
-
   </>
 );
 
--- src/to_impl.py
+++ src/to_impl.py
@@ -7,48 +7,13 @@ import os
 stopflag = False
 prog_pos = 35
 
-MODE_DEF = True  # answer with def
-MODE_TERM = False  # answer with term
-
-mode = MODE_DEF
-
 normal_chars = [chr(x) for x in range(ord("A"), ord("A") + 26)]
 normal_chars += [chr(x) for x in range(ord("a"), ord("a") + 26)]
 normal_chars += [str(x) for x in range(0, 9)]
 normal_chars += ' '
 
 
-def choose_set():
-    files = os.listdir('sets')
-
-    print("Found the following flashcard sets. If yours isn't here, add the .cards file to the sets folder.")
-    for filename in files:
-        if '.cards' in filename:
-            print("Set: " + filename.removesuffix('.cards'))
-
-    name = input("Type set name: ")
-    if name + '.cards' in files:
-        return name
-    else:
-        print("Set not found!")
-        return False
-
-
 def load_cards(name):
-    cards = {}
-    with open('sets/' + name + '.cards', encoding="utf-8") as f:
-        lines = f.readlines()
-
-    for line in lines:
-        line = line.strip()
-        if ' | ' not in line:
-            print("<" + line + "> did not contain ` | `")
-        term, definition = line.split(' | ')
-        if mode:
-            cards[term] = definition
-        else:
-            cards[definition] = term
-
     b_chars = []
     for word in cards.values():
         for char in word:
@@ -64,60 +29,6 @@ def load_progress(cards_name, cards):
         if input("Use existing progress (y / n): ").lower()[0] == "y":
             with open('sets/' + cards_name + '.progress') as f:
                 progress = json.load(f)
-
-    delkeys = []
-    addkeys = []
-    for key in progress:
-        if key not in cards:
-            delkeys.append(key)
-    for key in cards:
-        if key not in progress:
-            addkeys.append(key)
-
-    for key in delkeys:
-        progress.pop(key)
-    for key in addkeys:
-        progress[key] = {"mc": False, "or": False,
-                         "missed": False}  # multichoice, open response
-
-    return progress
-
-
-def ask_mc(term, cards, progress):
-    global stopflag
-    answers = [cards[term]]
-    for i in range(0, 3):
-        c = answers[0]
-        while c in answers:
-            c = random.choice(list(cards.values()))
-        answers.append(c)
-
-    random.shuffle(answers)
-
-    print()
-    print()
-    print("  " + term + " " * (prog_pos - len(term)) +
-          " " + str(round(progress * 100, 2)) + '%')
-    print()
-    a1 = '1: ' + answers[0]
-    a2 = '2: ' + answers[1]
-    a3 = '3: ' + answers[2]
-    a4 = '4: ' + answers[3]
-
-    ldif = len(a1) - len(a2)
-
-    print(a1, ' ' * -ldif + '  ', a3)
-    print()
-    print(a2, ' ' * ldif + '  ', a4)
-    print()
-
-    resp = ''
-    while resp not in '1234' or resp == '':
-        resp = input('> ')
-        if resp == 'stop':
-            stopflag = True
-            return False
-    return answers[int(resp) - 1].lower() == cards[term].lower()
 
 
 def ask_or(term, cards, bchars, progress):
@@ -142,35 +53,6 @@ def ask_or(term, cards, bchars, progress):
     return resp == cards[term]
 
 
-def p_correct():
-    print()
-    print()
-    print("+-----------------------------------------+")
-    print("|                                         |")
-    print("|                 Correct                 |")
-    print("|                                         |")
-    print("|                                         |")
-    print("|                                         |")
-    print("+-----------------------------------------+", end='')
-    sleep(1)
-
-
-def p_incorrect(canswer):
-    print()
-    print()
-    print("+-----------------------------------------+")
-    print("|                                         |")
-    print("|                Incorrect                |")
-    print("|                                         |")
-    l = len(canswer)
-    lb = len("-----------------------------------------")
-    a, b = math.floor((lb - l) / 2), math.ceil((lb - l) / 2),
-    print("|" + " " * a + canswer + " " * b + "|")
-    print("|                                         |")
-    print("+-----------------------------------------+", end='')
-    input()
-
-
 def p_done():
     print()
     print()
@@ -184,26 +66,17 @@ def p_done():
 def study(cards, progress, bchars):
     missed_words = []
     while True:
-        mc_list = []
-        for key, value in progress.items():
-            if not value["mc"]:
-                mc_list.append(key)
 
         if len(mc_list) > 0:
             term = random.choice(mc_list)
             if ask_mc(term, cards, 1 - len(mc_list)/len(progress)):
                 if stopflag:
                     break
-                p_correct()
-                if progress[term]["missed"]:
-                    progress[term]["missed"] = False
-                else:
-                    progress[term]["mc"] = True
+
             else:
                 if stopflag:
                     break
                 p_incorrect(cards[term])
-                progress[term]["missed"] = True
                 if term + ' / ' + cards[term] not in missed_words:
                     missed_words.append(term + ' / ' + cards[term])
 
@@ -243,9 +116,9 @@ def main():
     if cards_name:
         if input("Answer with (term / def): ").lower() == "term":
             mode = MODE_TERM
-        cards, bchars = load_cards(cards_name)
+
         progress = load_progress(cards_name, cards)
-        print_banner()
+
         missed_words = study(cards, progress, bchars)
         print("Your missed words: ", ', '.join(missed_words))
         if input("Save progress (y / n): ").lower()[0] == "y":
